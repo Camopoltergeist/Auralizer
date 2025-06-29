5804c923f450a13f39f161e72936cf36c71b9ea3
Camopoltergeist <camopoltergeist@outlook.com>
2025-06-29 13:09:21 +0300

    Hide OpenGL notification messages

--- src/init.cpp
+++ src/init.cpp
@@ -74,6 +74,7 @@ bool init_opengl(AppState* app_state) {
 	}
 
 	glDebugMessageCallback(debug_message_callback, nullptr);
+	glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DEBUG_SEVERITY_NOTIFICATION, 0, nullptr, GL_FALSE);
 	
 	SDL_GL_SetSwapInterval(1);
 
