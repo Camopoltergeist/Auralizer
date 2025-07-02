f9366374a7d6a35529088b6eab9effebc3c0c51e
Camopoltergeist <camopoltergeist@outlook.com>
2025-07-02 12:06:06 +0300

    Move AppState constructor and destructor to cpp file

---
+++ src/AppState.cpp
@@ -0,0 +1,52 @@
+#include "AppState.hpp"
+
+inline AppState::AppState() {
+	main_window = nullptr;
+	gl_context = nullptr;
+	vertex_buffer = 0;
+	index_buffer = 0;
+	vertex_array_object = 0;
+	vertex_shader = 0;
+	fragment_shader = 0;
+	pipeline = 0;
+	is_audio_file_selected = false;
+	audio_file_path = "";
+	audio_volume = 0.5f;
+	audio_engine = nullptr;
+	sound = nullptr;
+	is_imgui_window_open = false;
+}
+
+inline AppState::~AppState() {
+	if (sound != nullptr) {
+		ma_sound_uninit(sound);
+	}
+
+	if (audio_engine != nullptr) {
+		ma_engine_uninit(audio_engine);
+	}
+
+	if (vertex_array_object != 0) {
+		glDeleteVertexArrays(1, &vertex_array_object);
+	}
+
+	if (vertex_buffer != 0) {
+		glDeleteBuffers(1, &vertex_buffer);
+	}
+
+	if (index_buffer != 0) {
+		glDeleteBuffers(1, &index_buffer);
+	}
+
+	if (pipeline != 0) {
+		glDeleteProgramPipelines(1, &pipeline);
+	}
+
+	if (vertex_shader != 0) {
+		glDeleteProgram(vertex_shader);
+	}
+
+	if (fragment_shader != 0) {
+		glDeleteProgram(fragment_shader);
+	}
+}
