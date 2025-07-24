b9dc210d6049d0d41569f25e324ec21570ce25a9
Camopoltergeist <camopoltergeist@outlook.com>
2025-07-25 00:42:02 +0300

    Figured out move bullshit

--- src/gl/VertexArray.cpp
+++ src/gl/VertexArray.cpp
@@ -4,6 +4,28 @@
 
 VertexArray::VertexArray(GLuint gl_name) : gl_name(gl_name) { }
 
+VertexArray::VertexArray(VertexArray&& other) noexcept : gl_name(0)
+{
+	gl_name = other.gl_name;
+	other.gl_name = 0;
+}
+
+VertexArray& VertexArray::operator=(VertexArray& other)
+{
+	if (this == &other) {
+		return *this;
+	}
+
+	if (gl_name != 0) {
+		glDeleteVertexArrays(1, &gl_name);
+	}
+
+	gl_name = other.gl_name;
+	other.gl_name = 0;
+
+	return *this;
+}
+
 std::optional<VertexArray> VertexArray::create()
 {
 	GLuint vertex_array_object = 0;
@@ -21,12 +43,27 @@ std::optional<VertexArray> VertexArray::create()
 	glVertexArrayAttribFormat(vertex_array_object, 0, 2, GL_FLOAT, GL_FALSE, 0);
 	glVertexArrayAttribFormat(vertex_array_object, 1, 2, GL_FLOAT, GL_FALSE, sizeof(GLfloat) * 2);
 
-	VertexArray vertex_array(vertex_array_object);
-
-	return std::optional<VertexArray>(vertex_array);
+	return std::make_optional<VertexArray>(vertex_array_object);
 }
 
 VertexArray::~VertexArray()
 {
-	glDeleteVertexArrays(1, &gl_name);
+	if (gl_name != 0) {
+		glDeleteVertexArrays(1, &gl_name);
+	}
+}
+
+GLuint VertexArray::name() const
+{
+	return gl_name;
+}
+
+void VertexArray::enable_array_attrib(GLuint attrib_index) const
+{
+	glEnableVertexArrayAttrib(gl_name, attrib_index);
+}
+
+void VertexArray::set_attrib_format(GLuint attrib_index, GLint size, GLenum type, GLboolean normalized, GLuint relative_offset) const
+{
+	glVertexArrayAttribFormat(gl_name, attrib_index, size, type, normalized, relative_offset);
 }
--- src/gl/VertexArray.h
+++ src/gl/VertexArray.h
@@ -9,10 +9,18 @@ private:
 	GLuint gl_name;
 
 	VertexArray(GLuint gl_name);
+	VertexArray(const VertexArray&) = delete;
 
 public:
+	VertexArray(VertexArray&&) noexcept;
+	~VertexArray();
+	
+	VertexArray& operator=(VertexArray& other);
+	
 	static std::optional<VertexArray> create();
 
-	~VertexArray();
-};
+	GLuint name() const;
 
+	void enable_array_attrib(GLuint attrib_index) const;
+	void set_attrib_format(GLuint attrib_index, GLint size, GLenum type, GLboolean normalized, GLuint relative_offset) const;
+};
