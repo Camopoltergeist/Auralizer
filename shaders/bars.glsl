cd3bc6c8fb8378134dfaa9499687c4fd45e97dc6
Camopoltergeist <camopoltergeist@outlook.com>
2025-08-17 04:18:16 +0300

    Add bars shader

---
+++ shaders/bars.glsl
@@ -0,0 +1,25 @@
+#version 460 core
+
+in vec2 v_screen_uv;
+
+out vec4 frag_color;
+
+uniform sampler2D t;
+uniform vec2 fft_size;
+uniform vec2 viewport_size;
+
+void main() {
+    const vec4 color = vec4(1.0, 1.0, 1.0, 1.0);
+    const float bar_count = 100.0;
+
+    float bar_index = floor(bar_count * v_screen_uv.x);
+    float sampling_uv_x = bar_index / bar_count;
+
+    vec2 sampling_uv = vec2(sampling_uv_x, 0.5);
+
+    float amp = textureLod(t, sampling_uv, 0.0).r;
+
+    float s = step(v_screen_uv.y, amp);
+
+    frag_color = color * s;
+}
