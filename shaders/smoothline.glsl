f7ac67ffccb6818fb9f997ad6ae68ffb66900c13
Camopoltergeist <camopoltergeist@outlook.com>
2025-08-21 03:04:18 +0300

    Added color

--- shaders/smoothline.glsl
+++ shaders/smoothline.glsl
@@ -67,19 +67,37 @@ float distance_to_line(vec2 point_a, vec2 point_b, vec2 target) {
     return distance(target, closest_point);
 }
 
-float distance_to_segment_by_index(float segment_index, float segment_count) {
+float distance_to_segment_by_index(float segment_index, float segment_count, float channel) {
     const vec2 pixel_pos = v_screen_uv * viewport_size;
 
     float left_x = segment_index / segment_count;
     float right_x = (segment_index + 1.0) / segment_count;
 
-    float left_y = sample_index(segment_index, segment_count, 0.5);
-    float right_y = sample_index(segment_index + 1.0, segment_count, 0.5);
+    float left_y = sample_index(segment_index, segment_count, channel);
+    float right_y = sample_index(segment_index + 1.0, segment_count, channel);
 
     return distance_to_line(vec2(left_x, left_y) * viewport_size, vec2(right_x, right_y) * viewport_size, pixel_pos);
 }
 
+float a(float segment_index, float segment_count, float thickness, float channel) {
+    float current_d = distance_to_segment_by_index(segment_index, segment_count, channel);
+    float left_d = distance_to_segment_by_index(segment_index - 1.0, segment_count, channel);
+    float right_d = distance_to_segment_by_index(segment_index + 1.0, segment_count, channel);
+
+    float d = min(min(current_d, left_d), right_d);
+
+    const float half_thickness = thickness / 2.0;
+
+    return 1.0 - smoothstep(half_thickness - 0.5, half_thickness + 0.5, d);
+}
+
 void main() {
+    const vec4 left_color_low = vec4(0.5, 1.0, 0.0, 1.0);
+    const vec4 left_color_high = vec4(1.0, 0.0, 0.5, 1.0);
+
+    const vec4 right_color_low = vec4(0.5, 0.0, 1.0, 1.0);
+    const vec4 right_color_high = vec4(0.0, 1.0, 0.5, 1.0);
+
     const vec4 color = vec4(1.0, 1.0, 1.0, 1.0);
     const float bar_count = 100.0;
     const float thickness = 20.0;
@@ -87,13 +105,8 @@ void main() {
 
     float bar_index = floor(bar_count * v_screen_uv.x);
 
-    float current_d = distance_to_segment_by_index(bar_index, bar_count);
-    float left_d = distance_to_segment_by_index(bar_index - 1.0, bar_count);
-    float right_d = distance_to_segment_by_index(bar_index + 1.0, bar_count);
+    float left = a(bar_index, bar_count, thickness, 0.0);
+    float right = a(bar_index, bar_count, thickness, 1.0);
 
-    float d = min(min(current_d, left_d), right_d);
-
-    float s = 1.0 - smoothstep(half_thickness - 0.5, half_thickness + 0.5, d);
-
-    frag_color = color * s;
+    frag_color = mix(left_color_low, left_color_high, v_screen_uv.y) * left + mix(right_color_low, right_color_high, v_screen_uv.y) * right;
 }
